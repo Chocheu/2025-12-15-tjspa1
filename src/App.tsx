a75bf1c17b7bf949bd0fd4144f8558262cfc5fa5
tpitel <thibaud.pitel@agencedpc.fr>
2025-12-15 12:44:21 +0100

    button component

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Demat Breizh</div>
-}
-
-export default App
---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import Button from "../components/ui/Button/Button";
+
+function App() {
+  return (
+    <div>
+      Demat Breizh
+      <br />
+      <Button title="cancel" bgcolor="tomato" color="white"></Button>
+      <Button title="ok" bgcolor="tomato" color="green"></Button>
+    </div>
+  );
+}
+
+export default App;
