70858d09c9243ce0ac53f9ea364adf5f1e47fc0b
tpitel <thibaud.pitel@agencedpc.fr>
2025-12-16 11:54:39 +0100

    update app with new components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,42 +1,23 @@
-import { useEffect, useState } from "react";
-import Button from "../components/ui/Button/Button";
+import MemeForm from "../components/functionnal/MemeForm/MemeForm";
+import FlexH3Grow from "../components/layout/FlexH3Grow/FlexH3Grow";
+import FlexV1Grow from "../components/layout/FlexV1Grow/FlexV1Grow";
+import Footer from "../components/ui/Footer/Footer";
+import Header from "../components/ui/Header/Header";
+import MemeSvgViewer from "../components/ui/MemeSvgViewer/MemeSvgViewer";
+import Navbar from "../components/ui/Navbar/Navbar";
 
 const App: React.FC = () => {
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    // componentDidMount
-    // console.log("did mount");
-    // setCounter(0); // inutile mais pour l'exemple => useState(0) à la place
-    
-    // componentDidUpdate
-    console.log("counter effect", counter);
-
-    return () => {
-      // componentWillUnmount
-    };
-  }, [counter]);
-
   return (
     <div style={{ textAlign: "center" }}>
-      valeur de counter :{counter}
-      <hr />
-      <Button
-        onButtonClick={() => {
-          setCounter(counter - 1);
-        }}
-      >
-        -1
-      </Button>
-      <Button
-        bgColor="skyblue"
-        onButtonClick={() => {
-          setCounter(counter + 1);
-
-          console.log(counter);
-        }}
-      >
-        +1
-      </Button>
+      <FlexH3Grow>
+        <Header></Header>
+        <Navbar></Navbar>
+        <FlexV1Grow>
+          <MemeSvgViewer></MemeSvgViewer>
+          <MemeForm></MemeForm>
+        </FlexV1Grow>
+        <Footer></Footer>
+      </FlexH3Grow>
     </div>
   );
 };
