14e50004ae42bbca9419cf9dc0b4cad13f5b628e
tpitel <thibaud.pitel@agencedpc.fr>
2025-12-17 12:08:47 +0100

    make basepath a mandatory props

--- src/components/ui/MemeSvgViewer/MemeSvgViewer.store.tsx
+++ src/components/ui/MemeSvgViewer/MemeSvgViewer.store.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import type { StoreState } from "../../../store/store";
 
 interface IMemeSvgViewerProps {
-  basePath?: "/" | "";
+  basePath: "/" | "";
 }
 
 const MemeSvgViewer: React.FC<IMemeSvgViewerProps> = (props) => {
