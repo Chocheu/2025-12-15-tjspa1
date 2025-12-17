df05cad3b6384ab45604970e0cc45bc26a8e5e02
tpitel <thibaud.pitel@agencedpc.fr>
2025-12-17 16:48:48 +0100

    fix useless code

--- src/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
+++ src/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
@@ -1,23 +1,12 @@
-import React, { useEffect, useState } from "react";
 import styles from "./MemeSvgViewer.module.css";
 
 interface IMemeSvgViewerProps {
   children?: React.ReactNode;
 }
-interface IMemeSvgViewerState {
-  value?: undefined;
-}
 
 const MemeSvgViewer: React.FC<IMemeSvgViewerProps> = ({
   children = "MemeSvgViewer Component",
 }) => {
-  const [state, setState] = useState<IMemeSvgViewerState>({});
-  useEffect(() => {
-
-    return () => {
-    };
-  }, []);
-
   return (
     <div className={styles.MemeSvgViewer} data-testid="MemeSvgViewer">
       {children}
